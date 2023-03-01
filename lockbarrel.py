33408b669d5c1086473998f93aa7a084ff420d88
Vladislav/Владислав <killberos.tze@gmail.com>
2023-03-01 23:12:37 +0100

    Update lockbarrel.py

--- lockbarrel.py
+++ lockbarrel.py
@@ -46,35 +46,17 @@ class Lock_barrel():
         self.mainframe.rowconfigure(0, weight = 1)
         self.mainframe.pack(pady = (50,50), padx = (50,50))
         self.init_stuff()
-        
-    def change(self,idx,direction):
-        listidx=(self.digits.index(int(self.variables[idx].get()))+direction) % len(self.digits)
-        self.variables[idx].set(self.digits[listidx])
-        
+   
         
     def init_stuff(self):
-        #dirname=os.path.dirname(__file__)#path of this __file__ not the __main__
-        #imagepath=os.path.join(dirname, 'lockimages', "button.png")
-        #image = Image.open(imagepath)
-        #self.imageup=ImageTk.PhotoImage(image)
-        #self.imagedown=ImageTk.PhotoImage(image.rotate(180))
-        #self.digits=[0,1,2,3,4,5,6,7,8,9]
+
         self.variables=[]
         #self.index=[]
         for mm in range(0,self.no_rows):
             for nn in range(0,self.no_digits):
                 idx=mm*self.no_digits+nn
-                #self.index.append(idx)
-                #self.variables.append(tk.StringVar(self.root))
-                #self.variables[idx].set(self.digits[0])
-                #tk.Label(self.mainframe, textvariable=self.variables[idx], borderwidth=2,relief=tk.GROOVE).grid(row=mm*4+2,column=nn)
-                #tk.Button(self.mainframe, image=self.imageup, command=lambda lidx=idx, direction=+1: self.change(lidx,direction)).grid(row=mm*4+1,column=nn)#This may look magical, but here's what's happening. When you use that lambda to define your function, the open_this call doesn't get the value of the variable i at the time you define the function. Instead, it makes a closure, which is sort of like a note to itself saying "I should look for what the value of the variable i is at the time that I am called". Of course, the function is called after the loop is over, so at that time i will always be equal to the last value from the loop.
-#Using the i=i trick causes your function to store the current value of i at the time your lambda is defined, instead of waiting to look up the value of i later.
-#https://stackoverflow.com/questions/10865116/tkinter-creating-buttons-in-for-loop-passing-command-arguments
-                #tk.Button(self.mainframe, image=self.imagedown, command=lambda lidx=idx, direction=-1: self.change(lidx,direction)).grid(row=mm*4+3,column=nn)
                 self.variables.append(Rotate(parent=self.mainframe,direction='vertical',width=2,choice_list=self.digits,typevar=tk.IntVar()))
                 self.variables[idx].grid(row=mm,column=nn)
-            #if mm<self.no_rows-1:
-            #    tk.Label(self.mainframe, text='\t').grid(row=mm*4+4,column=0, columnspan=self.no_digits)
+      
 if __name__=='__main__':
     Lock_barrel.init_start(Lock_barrel())
